f9eaf946c4fffc15ad444cd26c2942f84e8a75db
sirSayed98 <mohamed.sayed8899@gmail.com>
2023-12-10 18:36:52 +0200

    feat: :tada: add delete employee

--- Client/Employee/list.js
+++ Client/Employee/list.js
@@ -1,22 +1,30 @@
-async function listAllEmployees(){
+async function listAllEmployees() {
   const res = await axios.get('http://localhost:3000/api/v1/employee/')
-  const { data }  = res.data
+  const { data } = res.data
 
-  const employeeTableBody = document.getElementById('employeeTable').getElementsByTagName('tbody')[0];
-  data.forEach(employee =>
-    {
-      console.log(employee);
-      const row = employeeTableBody.insertRow();
-      row.innerHTML = 
-      `<td>${employee.employee_id}</td>
+  const employeeTableBody = document.getElementById('table-body')
+  // clear  tablebody
+  employeeTableBody.innerHTML = ``
+  data.forEach(employee => {
+    console.log(employee)
+    const row = employeeTableBody.insertRow()
+    row.innerHTML = `<td>${employee.employee_id}</td>
        <td>${employee.employee_name}</td>
        <td>${employee.department_name}</td>
        <td>${employee.salary}</td>
         <td>
           <a href="./edit-employee.html?id=${employee.employee_id}" class="btn btn-sm btn-primary">Edit</a>
-          <a class="btn btn-sm btn-danger">Delete</a>
-        </td>`;
-    });
+          <a class="btn btn-sm btn-danger"  onclick="handleDeleteEmployee(${employee.employee_id})">Delete</a>
+        </td>`
+  })
+}
+
+async function handleDeleteEmployee(emp_id) {
+  let answer = confirm('Do you want to delete employee')
+  if (answer) {
+    await axios.delete(`http://localhost:3000/api/v1/employee/${emp_id}`)
+    listAllEmployees()
+  }
 }
 
 document.addEventListener('DOMContentLoaded', event => {
